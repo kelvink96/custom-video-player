e3c3c35b6080becf80df68d9570ef407e403f5cf
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2020-03-16 13:26:20 +0300

    # play/pause functionality working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,16 @@
-let video: any = document.querySelector('.video');
-let progress = document.querySelector('.progress-bar');
-let playPauseBtn: any = document.getElementById('play-pause');
-
+var video = document.querySelector('.video');
+var progress = document.querySelector('.progress-bar');
+var playPauseBtn = document.getElementById('play-pause');
 playPauseBtn.onclick = function () {
     togglePlayPause();
 };
-
 function togglePlayPause() {
     if (this.video.paused) {
         this.playPauseBtn.className = 'pause';
         this.video.play();
-    } else {
+    }
+    else {
         this.playPauseBtn.className = 'play';
         this.video.pause();
     }
 }
-
